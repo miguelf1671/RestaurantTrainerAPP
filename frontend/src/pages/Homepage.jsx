8ae748b5722b485e203d898db59fcef9bde23d23
miguel flores rammirez <deeznutz@miguels-Mini.home>
2023-12-12 22:36:57 -0500

    added cool buttons

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -6,9 +6,15 @@ function HomePage() {
       <div className="bg-blue-500  flex justify-center items-start h-screen">
         <div className="text-center px-40" style={{ marginTop: "20vh" }}>
           <h1 className=" font-mono font-bold text-2xl hover:underline decoration-wavy decoration-sky-500">
-            "Empowering Your Team: Training a New Employee Made as Simple as 1,
-            2, 3 for Effortless Onboarding!"
+            "Empowering Your Team: Training a New Employee Made Simple and
+            Effortless!"
           </h1>
+          <button className="bg-sky-400 rounded px-1 py-1 mr-6 mt-6 text-sky-700 hover:bg-sky-300 font-semibold">
+            Login
+          </button>
+          <button className="bg-sky-400 rounded px-1 py-1 ml-6 mt-6 text-sky-700 hover:bg-sky-300 font-semibold">
+            Sign Up
+          </button>
         </div>
       </div>
     </>
